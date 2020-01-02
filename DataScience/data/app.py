3acb0c78dd73a8153fcecfb979db214010b8460c
mforberg <mifor16@student.sdu.dk>
2020-01-02 14:04:01 +0100

    comment

--- DataScience/data/app.py
+++ DataScience/data/app.py
@@ -5,12 +5,14 @@ import os
 sc = SparkContext(master='local', appName='Upload CSV files', )
 ss = SparkSession(sc)
  
+# Mac makes retarded .DS_Store files in directories will fuck with data
 filePath = '/app/csv/.DS_Store'
 if os.path.exists(filePath):
     os.remove(filePath)
     print("DELETED")
 else:
     print("NOT DELETED")
+# Do stuff now
 
 dirPath = "/app/csv/"
 files = os.listdir(dirPath)
@@ -18,6 +20,6 @@ files = os.listdir(dirPath)
 pathToCSVFiles = "file://" + dirPath + "*.csv"
 df = ss.read.csv(pathToCSVFiles)
 pathToSave = "hdfs://namenode:9000/user/root/data/sofia.csv"
-df.write.csv(pathToSave)
+df.write.csv(pathToSave) 
 
 sc.stop()
