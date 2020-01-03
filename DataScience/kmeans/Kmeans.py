caea412fa98a59f0c55886bd586da26061adb231
mforberg <mifor16@student.sdu.dk>
2020-01-03 14:38:53 +0100

    julie, do the thing

--- DataScience/kmeans/Kmeans.py
+++ DataScience/kmeans/Kmeans.py
@@ -5,6 +5,7 @@ from pyspark.context import SparkContext
 from pyspark.sql.session import SparkSession
 
 from pyspark.ml.feature import VectorAssembler
+from pyspark.sql import functions as F
 
 
 
@@ -15,32 +16,8 @@ spark = SparkSession(sc)
 df = spark.read.option("header", "true").csv("/user/root/data/sofia.csv")
 df.printSchema()
 
-df.groupBy('sensor_id').agg({'P1' : 'mean'}).orderBy('sensor_id').show(100)
-test = df.select('lat', 'lon', 'P1').groupBy('lat', 'lon').show(100)
-#assembler = VectorAssembler(inputCols=[2, 3], outputCol="Coordinate")
+df.select('P2').orderBy('P2', ascending=False).show(100)
+#test = df.groupBy('sensor_id').agg({'P1' : 'min'}).orderBy('sensor_id')
+min_max_avg_df = df.groupBy('sensor_id').agg(F.min(df.P1),F.max(df.P1),F.avg(df.P1)).orderBy('sensor_id')
 
-#output = assembler.transform(df)
-
-#output.select("Coordinate", )
-
-# # Loads data.
-# dataset = spark.read.format("libsvm").load("data/mllib/sample_kmeans_data.txt")
-
-# # Trains a k-means model.
-# kmeans = KMeans().setK(2).setSeed(1)
-# model = kmeans.fit(dataset)
-
-# # Make predictions
-# predictions = model.transform(dataset)
-
-# # Evaluate clustering by computing Silhouette score
-# evaluator = ClusteringEvaluator()
-
-# silhouette = evaluator.evaluate(predictions)
-# print("Silhouette with squared euclidean distance = " + str(silhouette))
-
-# # Shows the result.
-# centers = model.clusterCenters()
-# print("Cluster Centers: ")
-# for center in centers:
-#     print(center)
+#print(test.show(100))
